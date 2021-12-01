43f9fd1d627e1f8658f3213d9e90a5cd8b99fc09
LauraMariaChiara <91607635+LauraMariaChiara@users.noreply.github.com>
2021-12-01 14:53:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@ using namespace std;
 #include "lib.h"
 
 int main(){
-  char a=a;
-  cin>>a;
-  if(conversione(a)== false ){
-    cout << "errore";
-  }else{
-    cout << a;
-  }
-  return 0;
+    char a=a;
+    cin>>a;
+    if(conversione(a)== false ){
+        cout << "errore";
+    }else{
+        cout << a;
+    }
+    return 0;
 }
